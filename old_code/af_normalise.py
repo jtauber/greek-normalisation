8ff396f87a9f5f49f0fdc80126907681d3f7a94b
James Tauber <jtauber@jtauber.com>
2019-06-26 23:22:41 +0200

    consolidated normalisation functions from old scripts

--- old_code/af_normalise.py
+++ old_code/af_normalise.py
@@ -4,17 +4,9 @@ import glob
 import re
 import sys
 
-from norm_data import PROCLITICS, ENCLITICS, ELISION, MOVABLE, PROPER_NOUNS
-
-
-ENCLITICS_NORM = {
-    strip_last_accent(word): word
-    for word in ENCLITICS
-}
 
 def convert(token):
 
-    norm = token
     norm = norm.lstrip("(")
     norm = norm.rstrip(".,·;)")
     if not norm:
@@ -27,47 +19,6 @@ def convert(token):
     if re.match("^[A-Za-zë]+[?:]?$", norm):
         return norm, ["latin"]
 
-    norm_code = []
-
-    # change graves to acutes
-    temp = grave_to_acute(norm)
-    if norm != temp:
-        norm_code.append("grave")
-    norm = temp
-
-    if norm not in PROPER_NOUNS:
-        if norm != norm.lower():
-            norm = norm.lower()
-            norm_code.append("capitalisation")
-
-    if norm in ELISION:
-        norm = ELISION[norm]
-        norm_code.append("elision")
-
-    if norm in MOVABLE:
-        norm = MOVABLE[norm]
-        norm_code.append("movable")
-
-    # strip last accent if two
-    temp = strip_last_accent_if_two(norm)
-    if norm != temp:
-        norm_code.append("extra")
-    norm = temp
-
-    if count_accents(norm) == 0:
-        if norm.lower() in PROCLITICS:
-            norm = norm.lower()
-            norm_code.append("proclitic")
-        elif norm.lower() in ENCLITICS_NORM:
-            norm = ENCLITICS_NORM[norm.lower()]
-            norm_code.append("enclitic")
-        elif norm in [  # known bugs
-        ]:
-            norm_code.append("bug?")
-        else:
-            norm_code.append("ERROR")
-
-    return norm, norm_code
 
 
 if __name__ == "__main__":
--- old_code/nyssa_normalise.py
+++ old_code/nyssa_normalise.py
@@ -2,60 +2,6 @@
 
 import sys
 
-from data import PROCLITICS, ENCLITICS, ELISION, MOVABLE, PROPER_NOUNS
-
-
-ENCLITICS_NORM = {
-    strip_last_accent(word): word
-    for word in ENCLITICS
-}
-
-def convert(token):
-
-    norm = token
-    norm_code = []
-
-    # change graves to acutes
-    temp = grave_to_acute(norm)
-    if norm != temp:
-        norm_code.append("grave")
-    norm = temp
-
-    if norm not in PROPER_NOUNS:
-        if norm != norm.lower():
-            norm = norm.lower()
-            norm_code.append("capitalisation")
-
-    if norm in ELISION:
-        norm = ELISION[norm]
-        norm_code.append("elision")
-
-    if norm in MOVABLE:
-        norm = MOVABLE[norm]
-        norm_code.append("movable")
-
-    # strip last accent if two
-    temp = strip_last_accent_if_two(norm)
-    if norm != temp:
-        norm_code.append("extra")
-    norm = temp
-
-    if count_accents(norm) == 0:
-        if norm.lower() in PROCLITICS:
-            norm = norm.lower()
-            norm_code.append("proclitic")
-        elif norm.lower() in ENCLITICS_NORM:
-            norm = ENCLITICS_NORM[norm.lower()]
-            norm_code.append("enclitic")
-        elif norm in [  # known bugs
-        ]:
-            norm_code.append("bug?")
-        else:
-            print("*** ERROR ***", file=sys.stderr)
-            print(norm, file=sys.stderr)
-
-    return norm, norm_code
-
 
 if __name__ == "__main__":
     with open("tokens.txt") as f:
