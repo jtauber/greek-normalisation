829caa204721797edbcf447e76ab0d86b252a1c9
James Tauber <jtauber@jtauber.com>
2019-07-04 21:58:49 +0200

    more prep for release

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 from setuptools import setup
 
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+
 setup(
     name="greek-normalisation",
-    version="0.0",
+    version="0.1",
     description="Python 3 utilities for validating and normalising Ancient Greek text",
     url="http://github.com/jtauber/greek-normalisation",
     author="James Tauber",
     author_email="jtauber@jtauber.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license="MIT",
     packages=["greek_normalisation"],
     zip_safe=False,
