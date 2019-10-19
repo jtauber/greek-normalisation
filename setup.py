348295f00165aeaade675fdcd0c2605b1bb4c2bd
James Tauber <jtauber@jtauber.com>
2019-10-19 01:12:00 -0400

    update version in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="greek-normalisation",
-    version="0.1",
+    version="0.2",
     description="Python 3 utilities for validating and normalising Ancient Greek text",
     url="http://github.com/jtauber/greek-normalisation",
     author="James Tauber",
